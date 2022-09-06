ced1a06866398c77baa3b07b5d49388e8acc2243
amoriotechnology <madhu@amoriotech.com>
2022-09-06 15:53:42 +0530

    first commit

---
+++ image.php
@@ -0,0 +1,52 @@
+<?php 
+
+if($_REQUEST['id'] == 1){
+   
+    echo "<img style='min-height: 400px; width: 550px;' src='assets/img/granite/design/image_design/1.png' alt='design'>";
+} 
+elseif($_REQUEST['id'] == 2){
+
+    echo "<img style='min-height: 400px; width: 550px;' src='assets/img/granite/design/image_design/2.png' alt='design'>";
+}
+elseif($_REQUEST['id'] == 3){
+
+    echo "<img style='min-height: 400px; width: 550px;' src='assets/img/granite/design/image_design/3.png' alt='design'>";
+}
+elseif($_REQUEST['id'] == 4){
+
+    echo "<img style='min-height: 400px; width: 550px;' src='assets/img/granite/design/image_design/4.png' alt='design'>";
+}
+elseif($_REQUEST['id'] == 5){
+
+    echo "<img style='min-height: 400px; width: 550px;' src='assets/img/granite/design/image_design/5.png' alt='design'>";
+}
+elseif($_REQUEST['id'] == 6){
+
+    echo "<img style='min-height: 400px; width: 550px;' src='assets/img/granite/design/image_design/6.png' alt='design'>";
+}
+elseif($_REQUEST['id'] == 7){
+
+    echo "<img style='min-height: 400px; width: 550px;' src='assets/img/granite/design/image_design/7.png' alt='design'>";
+}
+elseif($_REQUEST['id'] == 8){
+
+    echo "<img style='min-height: 400px; width: 550px;' src='assets/img/granite/design/image_design/8.png' alt='design'>";
+}
+elseif($_REQUEST['id'] == 9){
+
+    echo "<img style='min-height: 400px; width: 550px;' src='assets/img/granite/design/image_design/9.png' alt='design'>";
+}
+elseif($_REQUEST['id'] == 10){
+
+    echo "<img style='min-height: 400px; width: 550px;' src='assets/img/granite/design/image_design/10.png' alt='design'>";
+}
+elseif($_REQUEST['id'] == 11){
+
+    echo "<img style='min-height: 400px; width: 550px;' src='assets/img/granite/design/image_design/11.png' alt='design'>";
+}
+elseif($_REQUEST['id'] == 12){
+
+    echo "<img style='min-height: 400px; width: 550px;' src='assets/img/granite/design/image_design/12.png' alt='design'>";
+}
+
+?>
